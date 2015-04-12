fa04b0ce3834cd13b378c688cdeb30b10e6cb44e
jakeoney <jakeoney09@gmail.com>
2015-04-12 16:56:32 -0500

    Parts 0 and 1 work. Off by a little from having part 2(1) working. I think part 2(50) will work once I fix 2(1).

--- Smart4/src/NNImpl.java
+++ Smart4/src/NNImpl.java
@@ -11,26 +11,26 @@ public class NNImpl{
 	public ArrayList<Node> inputNodes=null;//list of the output layer nodes.
 	public ArrayList<Node> hiddenNodes=null;//list of the hidden layer nodes
 	public Node outputNode=null;//the output node
-	
+
 	public ArrayList<Instance> trainingSet=null;//the training set
-	
+
 	Double learningRate=1.0; // variable to store the learning rate
 	int maxEpoch=1; // variable to store the maximum number of epochs
-	
+
 	/**
- 	* This constructor creates the nodes necessary for the neural network
- 	* Also connects the nodes of different layers
- 	* After calling the constructor the last node of both inputNodes and  
- 	* hiddenNodes will be bias nodes. The other nodes of inputNodes are of type
- 	* input. The remaining nodes are of type sigmoid. 
- 	*/
-	
+	 * This constructor creates the nodes necessary for the neural network
+	 * Also connects the nodes of different layers
+	 * After calling the constructor the last node of both inputNodes and  
+	 * hiddenNodes will be bias nodes. The other nodes of inputNodes are of type
+	 * input. The remaining nodes are of type sigmoid. 
+	 */
+
 	public NNImpl(ArrayList<Instance> trainingSet, int hiddenNodeCount, Double learningRate, int maxEpoch, Double [][]hiddenWeights, Double[] outputWeights)
 	{
 		this.trainingSet=trainingSet;
 		this.learningRate=learningRate;
 		this.maxEpoch=maxEpoch;
-		
+
 		//input layer nodes
 		inputNodes=new ArrayList<Node>();
 		int inputNodeCount=trainingSet.get(0).attributes.size();
@@ -39,11 +39,11 @@ public class NNImpl{
 			Node node=new Node(0);
 			inputNodes.add(node);
 		}
-		
+
 		//bias node from input layer to output
 		Node biasToHidden=new Node(1);
 		inputNodes.add(biasToHidden);
-		
+
 		//hidden layer nodes
 		hiddenNodes=new ArrayList<Node> ();
 		for(int i=0;i<hiddenNodeCount;i++)
@@ -55,17 +55,17 @@ public class NNImpl{
 				NodeWeightPair nwp=new NodeWeightPair(inputNodes.get(j),hiddenWeights[i][j]);
 				node.parents.add(nwp);
 			}
-			
+
 			hiddenNodes.add(node);
 		}
-		
+
 		//bias node from hidden layer to output
 		Node biasToOutput=new Node(3);
 		hiddenNodes.add(biasToOutput);
 
 		//Output node
 		outputNode=new Node(4);
-		
+
 		//Connecting hidden layer nodes with output node
 		for(int i=0;i<hiddenNodes.size();i++)
 		{
@@ -73,13 +73,13 @@ public class NNImpl{
 			outputNode.parents.add(nwp);
 		}
 	}
-	
+
 	/**
 	 * Get the output from the neural network for a single instance
 	 * 
 	 * The parameter is a single instance
 	 */
-	
+
 	public Double calculateOutputForInstance(Instance inst)
 	{
 		// TODO: add code here
@@ -88,7 +88,7 @@ public class NNImpl{
 		for(int i = 0; i < inst.attributes.size(); i++){
 			inputNodes.get(i).setInput(inst.attributes.get(i));
 		}
-		
+
 		//for each hidden node, calculate output, use those as inputs into output node
 		//then calculate output value at output node
 		for(int i = 0; i < hiddenNodes.size(); i++){
@@ -103,24 +103,54 @@ public class NNImpl{
 		return output;
 		//return outputNode.getOutput();
 	}
-	
 
-	
-	
-	
+
+
+
+
 	/**
 	 * Train the neural networks with the given parameters
 	 * 
 	 * The parameters are stored as attributes of this class
 	 */
-	
+
 	public void train()
 	{
-		// TODO: add code here
-		
-		
-		
-	}
-	
+		double output;
+		double teacher;
+		double error;
+		double value = 0;
+		double adjustedWeight = 0;
 
+		// TODO: add code here
+
+		for(int i = 0; i < 1/*this.maxEpoch*/; i++){
+			for(Instance e : trainingSet){
+				output = calculateOutputForInstance(e);
+				teacher = e.classValue;
+				error = teacher - output;
+				
+				//backward pass
+				//update hidden -> output
+				for(int j = 0; j < outputNode.parents.size(); j++){
+					NodeWeightPair hidden = outputNode.parents.get(j);
+					adjustedWeight = this.learningRate * hidden.node.getOutput() * error * output *(1-output);
+					value = hidden.weight * error * output * (1 - output);
+					
+					//update input -> hidden
+					if(hidden.node.parents != null){
+						for(int k = 0; k < hidden.node.parents.size(); k++){
+							NodeWeightPair inputNode = hidden.node.parents.get(k);
+							inputNode.weight += this.learningRate * inputNode.node.getOutput() * hidden.node.getOutput() *
+									(1 - hidden.node.getOutput()) * value;
+						}
+					}
+					
+					hidden.weight += adjustedWeight;
+					//value += adjustedWeight * error * output * (1 - output);
+				}
+			}
+		}
+
+	}
 }
